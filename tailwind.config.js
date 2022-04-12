3f1a199f43b594fcea71e3795818bd9cde517b4c
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-12 16:24:07 -0300

    Next-crud - Componente de Botao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
